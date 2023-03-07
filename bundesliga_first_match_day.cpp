7e3389f5f85267e1e761d1f12a46f2354747851b
Lukasz-334 <82501491+Lukasz-334@users.noreply.github.com>
2023-03-07 22:08:00 +0100

    Create bundesliga_first_match_day.cpp

---
+++ bundesliga_first_match_day.cpp
@@ -0,0 +1,213 @@
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
+
+class Team {
+protected:
+    std::string teamName;
+    int goalsScored = 0;
+    int goalsConceded = 0;
+    int numberOfMatches = 0;
+    int matchesWon = 0;
+    int matchDrawn = 0;
+    int matchesLost = 0;
+    int point = 0;
+    int differenceOfGoals = 0;
+
+public:
+    Team() = default;
+    Team(std::string t, int gs = 0, int gc = 0)
+        : teamName{t} {
+        if (gs >= 0) {
+            if (gs > gc) {
+                point += 3;
+                matchesWon++;
+            } else if (gs == gc) {
+                point++;
+                matchDrawn++;
+            } else {
+                matchesLost++;
+            }
+
+            goalsScored += gs;
+            goalsConceded += gc;
+            differenceOfGoals = goalsScored - goalsConceded;
+            numberOfMatches++;
+        }
+    }
+
+    std::string getTeamName() const {
+        return teamName;
+    }
+    int getGoalsScored() const {
+        return goalsScored;
+    };
+    int getGoalsConceded() const {
+        return goalsConceded;
+    };
+    int getNumberOfMatches() const {
+        return numberOfMatches;
+    };
+    int getMatchesWon() const {
+        return matchesWon;
+    };
+    int getMatchDrawn() const {
+        return matchDrawn;
+    };
+    int getMatchesLost() const {
+        return matchesLost;
+    };
+    int getPoint() const {
+        return point;
+    };
+    int getDifferenceOfGoals() const {
+        return differenceOfGoals;
+    }
+};
+
+class Data {
+protected:
+    std::string firstTeam;
+    std::string secondTeam;
+    int firstTeamPoint;
+    int secondTeamPoint;
+    const std::string str;
+
+public:
+    Data() = default;
+    Data(std::string&& data)
+        : str{std::move(data)} {
+        dataAssignment();
+    }
+
+    void validate(int& num, std::string& s) {
+        if (std::isdigit(s.at(0))) {
+            num = std::stoi(s);
+        } else {
+            num = -1;
+        }
+    }
+
+    void dataAssignment() {
+        std::string temp;
+        int colonPosition = str.find(':');
+        int dashPosition = str.rfind('-');
+        temp = str.substr(0, colonPosition);
+        validate(firstTeamPoint, temp);
+        temp = str.substr(colonPosition + 1, str.find(' ') - colonPosition - 1);
+        validate(secondTeamPoint, temp);
+        firstTeam = str.substr(str.find(' ') + 1, dashPosition - str.find(' ') - 2);
+        secondTeam = str.substr(dashPosition + 2);
+    }
+    std::string getFirstTeam() const {
+        return firstTeam;
+    }
+    std::string getSecondTeam() const {
+        return secondTeam;
+    }
+    int getFirstTeamPoint() const {
+        return firstTeamPoint;
+    }
+    int getSecondTeamPoint() const {
+        return secondTeamPoint;
+    }
+};
+
+class Bundesliga : public Team,
+                   public Data {
+private:
+    std::vector<Team> collect;
+    std::vector<int> position;
+
+public:
+    std::string table(std::vector<std::string> results) {
+        position.resize(18, 1);
+        for (std::string el : results) {
+            Data data(std::move(el));
+            Team team1(data.getFirstTeam(), data.getFirstTeamPoint(), data.getSecondTeamPoint());
+            collect.push_back(team1);
+            Team team2(data.getSecondTeam(), data.getSecondTeamPoint(), data.getFirstTeamPoint());
+            collect.push_back(team2);
+        }
+
+        sortTable();
+        teamPosition();
+        return print();
+    }
+
+    std::string print() {
+        std::string str;
+        std::stringstream ss;
+        int current = 0;
+
+        for (const Team& team : collect) {
+            ss << std::right << std::setw(2) << position[current] << '.' << " " << std::left << std::setw(30) << team.getTeamName()
+
+               << team.getNumberOfMatches()
+               << "  " << team.getMatchesWon()
+               << "  " << team.getMatchDrawn()
+               << "  " << team.getMatchesLost()
+               << "  " << team.getGoalsScored()
+               << ":" << team.getGoalsConceded()
+               << "  " << team.getPoint()
+               << std::endl;
+            current++;
+        }
+        std::string solv = ss.str();
+        solv.pop_back();
+        return solv;
+    }
+
+    void sortTable() {
+        std::sort(
+            collect.begin(), collect.end(), [](auto first, auto second) {
+                if (first.getPoint() > second.getPoint()) {
+                    return true;
+                }
+                if (first.getPoint() < second.getPoint()) {
+                    return false;
+                }
+
+                if (first.getPoint() == second.getPoint()) {
+                    if (first.getDifferenceOfGoals() > second.getDifferenceOfGoals()) {
+                        return true;
+                    }
+                    if (first.getDifferenceOfGoals() < second.getDifferenceOfGoals()) {
+                        return false;
+                    }
+                    if (first.getDifferenceOfGoals() == second.getDifferenceOfGoals()) {
+                        if (first.getGoalsScored() > second.getGoalsScored()) {
+                            return true;
+                        }
+                        if (first.getGoalsScored() < second.getGoalsScored()) {
+                            return false;
+                        }
+                    }
+                }
+                std::string str1 = std::move(first.getTeamName());
+                std::string str2 = std::move(second.getTeamName());
+
+                std::transform(str1.begin(), str1.end(), str1.begin(), [](unsigned char c) {
+                    return std::toupper(c);
+                });
+                std::transform(str2.begin(), str2.end(), str2.begin(), [](unsigned char c) {
+                    return std::toupper(c);
+                });
+                return str1 < str2;
+            });
+    }
+
+    void teamPosition() {
+        std::iota(position.begin(), position.end(), 1);
+        for (size_t i = 0; i < collect.size() - 1; i++) {
+            if ((collect[i].getPoint() == collect[i + 1].getPoint()) && (collect[i].getDifferenceOfGoals() == collect[i + 1].getDifferenceOfGoals()) && (collect[i].getGoalsScored() == collect[i + 1].getGoalsScored())) {
+                position[i + 1] = position[i];
+            }
+        }
+    }
+};
